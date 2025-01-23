700a96466b37470fe999ce0ff2a08e97d9a0b8a6
Ebe-N <nexercomputer@outlook.com>
2025-01-24 00:35:19 +0300

    Completed the initial structure of the forms

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -10,6 +10,9 @@ export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup: FormGroup;
 
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
@@ -19,13 +22,48 @@ export class CheckoutComponent implements OnInit {
         firstName: [''],
         lastName: [''],
         email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
       })
     });
+  }
+
+  copyShippingAddressToBillingAddress(event: Event) {
+    // Cast event.target to HTMLInputElement to access 'checked'
+    const isChecked = (event.target as HTMLInputElement).checked;
+    if (isChecked) {
+      // Copy shipping address to billing address
+      this.checkoutFormGroup.get('billingAddress')?.setValue(
+        this.checkoutFormGroup.get('shippingAddress')?.value
+      );
+    } else {
+      // Reset billing address
+      this.checkoutFormGroup.get('billingAddress')?.reset();
+    }
   }
 
   onSubmit(): void {
     // Log the form values when the form is submitted
     console.log('Handling the submit button');
     console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log('The email address is ' + this.checkoutFormGroup.get('customer')?.value.email);
   }
 }
