a17b93457edd8862ad4d477d2f54232af38f9fa3
Ebe-N <nexercomputer@outlook.com>
2025-02-04 00:04:11 +0300

    Added the form validation

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ɵNOT_FOUND_CHECK_ONLY_ELEMENT_INJECTOR } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Luv2ShopFormService } from '../../services/luv2-shop-form.service';
 import { Country } from '../../common/country';
 import { State } from '../../common/state';
@@ -32,9 +32,10 @@ export class CheckoutComponent implements OnInit {
     // Initialize the form group
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('',
+                               [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$)')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -88,6 +89,10 @@ export class CheckoutComponent implements OnInit {
     )
   }
 
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName');}
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName');}
+  get email() { return this.checkoutFormGroup.get('customer.email');}
+
   copyShippingAddressToBillingAddress(event: Event) {
     // Cast event.target to HTMLInputElement to access 'checked'
     const isChecked = (event.target as HTMLInputElement).checked;
@@ -111,6 +116,11 @@ export class CheckoutComponent implements OnInit {
   onSubmit(): void {
     // Log the form values when the form is submitted
     console.log('Handling the submit button');
+
+    if (this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log('The email address is ' + this.checkoutFormGroup.get('customer')?.value.email);
   
@@ -178,3 +188,5 @@ export class CheckoutComponent implements OnInit {
     )
   }
 }
+
+// Verify the validation rules for the form
